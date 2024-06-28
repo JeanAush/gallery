962bff0f0860cc2334ffbf1801eedc693c3b1482
JeanAush <natwolijean@gmail.com>
2024-06-28 13:54:41 +0300

    updates to the test branch

--- test/serverTest.js
+++ test/serverTest.js
@@ -1,26 +1,30 @@
-process.env.NODE_ENV = 'test';   
+process.env.NODE_ENV = "test";
 
-var chai = require('chai');
-var chaiHttp = require('chai-http');
-
-var server = require('../server');
+var chai = require("chai");
+var chaiHttp = require("chai-http");
+var server = require("../server");
 var should = chai.should();
 var expect = chai.expect;
 
 chai.use(chaiHttp);
 
-describe('Photos', function(){
+describe("Photos", function () {
+  // Close the server after all tests are done
+  after(function (done) {
+    server.close();
+    done();
+  });
 
-
-    it('should list ALL photos on / GET', function(done){
-        this.timeout(60000);
-        chai.request(server)
-        .get('/')
-        .end(function(err,res){
-            res.should.have.status(200);
-            res.should.be.html;
-            res.body.should.be.a('object')
-            done();
-        })
-    });
-})
+  it("should list ALL photos on / GET", function (done) {
+    this.timeout(60000); // Set timeout for the test
+    chai
+      .request(server)
+      .get("/")
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        res.body.should.be.a("object");
+        done();
+      });
+  });
+});
